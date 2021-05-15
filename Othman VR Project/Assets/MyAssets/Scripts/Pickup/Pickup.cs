52b3212e602c8df16898aefbc0af170d4377be8f
Othman Abdulsalam <othmanmabdulsalam@gmail.com>
2021-05-15 15:45:31 +0100

    pickup object works
    basically done with all the features i wanted to do; the bug still exists but really cba fixing it

---
+++ Othman VR Project/Assets/MyAssets/Scripts/Pickup/Pickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pickup : MonoBehaviour
+{
+    public float rotationSpeed = 100.0f;// rotation speed of the pickup
+    // Update is called once per frame
+    void Update()
+    {
+        rotateObject();
+    }
+
+    void rotateObject()
+    {
+        transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
+    }
+
+
+     void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player") // player has collected the object, they've finished the maze
+        {
+            // load the menu
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
